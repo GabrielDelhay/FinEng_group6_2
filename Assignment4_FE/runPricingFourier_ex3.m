b76b4a2d74b699d7fad64bfc7330367d1617ed27
stefanodeamici2003-jpg <stefanodeamici2003@gmail.com>
2026-04-22 14:50:40 +0200

    ex_4

--- Assignment4_FE/runPricingFourier_ex3.m
+++ Assignment4_FE/runPricingFourier_ex3.m
@@ -7,7 +7,7 @@ clear; clc; close all;
 %% Bootstrap
 formatDate = 'dd/mm/yyyy';
 maturity = datenum('15-Feb-2009');
-[datesSet, ratesSet] = readExcelDataOS('MktData_CurveBootstrap.xls', formatDate);
+[datesSet, ratesSet] = readExcelData('MktData_CurveBootstrap.xls', formatDate);
 [dates, discounts, zeroRates] = bootstrap(datesSet, ratesSet);
 
 %% Exercise 3 - Pricing
@@ -65,3 +65,29 @@ fprintf('Quadrature:  %.4f  %.4f  %.4f\n', C_quad);
 fprintf('Residuals:  %.4f  %.4f  %.4f\n', C_residuals);
 fprintf('MonteCarlo:  %.4f  %.4f  %.4f\n', C_mc);
 fprintf('FFT:  %.4f  %.4f  %.4f\n', C_fft);
+
+%% ex 4
+
+sigma = 0.20;  kappa = 1;  eta = 3;  alpha = 1/2;
+x_vec = (-0.25:0.01:0.25).';
+for j = 1:length(x_vec)
+    x = x_vec(j);% Quadrature method
+    Integrand = @(u) real(lewisIntegrand_ex4(u, x, sigma, kappa, eta, T, alpha));
+    I_q = 2 * quadgk(Integrand, 0, Inf); % Integrand is even in the complex sense
+    C_quad(j) = B * F0 * (1 - exp(-x/2) / (2*pi) * I_q);
+end
+% Compute FFT
+N = 2^12; du = 0.05;
+[x_grid, I_fft]  = compute_FFT_ex4(sigma, kappa, eta, T, alpha, N, du);
+x_targets = x_vec;                      % la tua griglia -25% .. +25%
+I_interp = interp1(x_grid, I_fft, x_targets, 'spline');
+C_fft = B * F0 * (1 - exp(-x_targets/2) ./ (2*pi) .* I_interp);
+% Compute MC
+C_mc_ = priceMC_ex4(x_vec, F0, B, sigma, kappa, eta, T, 1e6, alpha);
+
+
+%% results
+
+fprintf('Quadrature:  %.4f\n', C_quad);
+fprintf('MonteCarlo:  %.4f\n', C_mc_);
+fprintf('FFT:  %.4f\n', C_fft);
